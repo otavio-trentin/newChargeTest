1d795664e0574d6c522dc26684c4f6126d2ec579
Otavio Trentin <otavio_trentin@hotmail.com>
2022-11-10 10:44:53 -0300

    solucao teste 1

---
+++ interviewCode.py
@@ -0,0 +1,44 @@
+### comunication
+### tabela padrão de comunicação
+# Coluna 1: Endereço do registrador em decimal
+# Coluna 2: Endereço do registrador em hexadecimal 
+# Coluna 3: Número de bytes para formar o dado completo
+# Coluna 4: Descrição dos dados nos registradores em questão 
+# Coluna 5: Forma de conversão
+# Coluna 6: Após tratamento, qual o tipo de dado para conversão
+
+###requisicao em hex
+# Byte1 - Unidade do equipamento
+# Byte2 - Tipo de função a ser realizada (03 = leitura, 06 = gravação)
+# Byte3 - Primeira parte do endereço do registrador
+# Byte4 - Segunda parte do endereço do registrador
+# Byte5 - Primeira parte do número de registradores a serem lidos em sequência
+# Byte6 - Segunda parte do número de registradores a serem lidos em sequência
+# Byte7 e Byte8 = número de confirmação calculado a partir de uma tabela CRC (não importante para agora)
+
+
+# Teste 1:
+# A partir da solicitação: solicitação = “0503c5700006f95b” Utilizando a tabela 2, o que foi solicitado?
+###########
+# solicitado ao equipamento numero 05, funcao 03 (leitura), com endereco hexadecimal C570,
+# com numero de registrador a serem lidos 0006 e numero de confirmacao f95b!
+###########
+# Obtivemos a resposta: “05030c000000e000000176000000e55726'”
+# Utilizando a linguagem Python3, é solicitado ao candidato tratar e traduzir a resposta do equipamento.
+
+class deviceResponse():
+    def __init__(self, msg:str):
+        self.HexMSG = msg
+    def extractMSG(self):
+        self.device = int(self.HexMSG[:2], 16) 
+        self.function = int(self.HexMSG[2:4], 16)
+        self.bytesResponse = int(self.HexMSG[4:6], 16) ## value will change depending on klaus response
+        self.response = [] ## join by registers
+        for i in range(6,6+self.bytesResponse*2,4): # i start from the 8th and go until the end of the double of the number of bytes response, the step is 4 cause this is the amount of bits in a register
+            self.response.append(int(self.HexMSG[i:i+4], 16)) 
+        self.confirmation = int(self.HexMSG[i:], 16)
+
+        
+resposta = deviceResponse("05030c000000e000000176000000e55726")
+resposta.extractMSG()
+print(resposta.response)
